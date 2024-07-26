b7fbc92244d93db2e7da82971290b09009c7dc32
Tizord-Linux <thiagowpsousa@gmail.com>
2024-07-26 15:06:32 -0300

    Aula 3 - Criando rotas com CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,72 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
+def test_root_deve_retornar_ok_e_ola_mundo(client):
+    # client = TestClient(app)  # Arrange
 
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá, mundo'}  # Assert
+
+
+def test_create_user(client):
+    # Arrange
+
+    response = client.post(
+        '/user/',
+        json={
+            'username': 'teste_username',
+            'password': 'password',
+            'email': 'teste@teste.com',
+        },
+    )  # Act
+    # Asset 1 - Voltou o status code correto?
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    # Assert 2 - Validar o UserPublic
+
+    assert response.json() == {
+        'username': 'teste_username',
+        'email': 'teste@teste.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'teste_username',
+                'email': 'teste@teste.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'teste_username2',
+            'email': 'teste2@teste.com',
+            'password': '123',
+            'id': 1,
+        },
+    )
+
+    assert response.json() == {
+        'username': 'teste_username2',
+        'email': 'teste2@teste.com',
+        'id': 1,
+    }
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
